dce3fabdce876a9706fb768134d5f94dc65a9da0
1 <unknown-user@fe80::ed31:eb42:e36a:d720%4>
2015-04-26 14:59:13 +0600

    playercript modification

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+
 
 public class PlayerScript : MonoBehaviour {
+	public float playerspeed = 2.0f;
+	private float currentSpeed = 0.0f;
+
+	public List<KeyCode> upButton;
+	public List<KeyCode> downButton;
+	public List<KeyCode> leftButton;
+	public List<KeyCode> rightButton;
+
+	private Vector3 lastMovement = new Vector3();
 
 	// Use this for initialization
 	void Start () {
